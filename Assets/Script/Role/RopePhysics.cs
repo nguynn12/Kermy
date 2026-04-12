03c8b3a910b05d992af75bada5ad7a2ef382bb71
PhuocTran <2312719@dlu.edu.vn>
2026-04-12 21:48:59 +0700

    add rope physics

---
+++ Assets/Script/Role/RopePhysics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RopePhysicsSoft : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rbA;
+    [SerializeField] private Rigidbody2D rbB;
+
+    private DistanceJoint2D joint;
+
+    void Start()
+    {
+        if (rbA == null || rbB == null)
+        {
+            Debug.LogError("Chưa gán Rigidbody!");
+            return;
+        }
+
+        joint = gameObject.AddComponent<DistanceJoint2D>();
+
+        joint.enableCollision = true;
+        joint.connectedBody = rbB;
+
+        joint.autoConfigureDistance = false;
+        joint.maxDistanceOnly = true;
+
+        joint.distance = Vector2.Distance(rbA.position, rbB.position);
+    }
+}
